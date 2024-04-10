69fce1132a71d3f9e69fd8135166fe49175b5485
Rachna90 <98342227+Rachna90@users.noreply.github.com>
2024-04-10 19:20:19 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#ibox-open').on("click", function(){
+        $("#iboxright_slide-right").toggle(1000);
+    })
+});
+
+document.getElementById('myBtn').onclick = myFunction;
+
+function myFunction(){
+    document.getElementById('demo').innerHTML= "I anjndfnk";
+}
+
